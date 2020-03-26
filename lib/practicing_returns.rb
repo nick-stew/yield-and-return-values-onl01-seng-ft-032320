f84785aa5fc7be759288c11f355b19daa6360a1b
John Stewart <jnicklausstewart@yahoo.com>
2020-03-26 17:26:37 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,15 +1,4 @@
-require 'pry'
 
-def hello(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-end
-
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 
 require 'pry'
 
