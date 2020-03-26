a824ef0919889a3e099ffa078914b0f558d0cc97
John Stewart <jnicklausstewart@yahoo.com>
2020-03-26 17:26:20 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -8,6 +8,19 @@ def hello(array)
   end
 end
 
-binding.pry
+
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 
+require 'pry'
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" 
